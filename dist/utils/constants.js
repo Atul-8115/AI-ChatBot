b59f5f5f08736a329576fab464af883375698943
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-05-25 20:18:39 +0530

    refactor: Update CORS origin configuration to allow specific origins

--- dist/utils/constants.js
+++ dist/utils/constants.js
@@ -1,3 +1,3 @@
-// export const COOKIE_NAME = "auth_token"
-export const COOKIE_NAME = "_vercel_jwt";
+export const COOKIE_NAME = "auth_token";
+// export const COOKIE_NAME = "_vercel_jwt"
 //# sourceMappingURL=constants.js.map
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
-// export const COOKIE_NAME = "auth_token"
-export const COOKIE_NAME = "_vercel_jwt"
+export const COOKIE_NAME = "auth_token"
+// export const COOKIE_NAME = "_vercel_jwt"
