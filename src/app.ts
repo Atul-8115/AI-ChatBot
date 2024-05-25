025a215c9f192cce0102ae661e90fabaeda5e7a0
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-05-25 16:55:08 +0530

    refactor: Update CORS origin to allow specific origins

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,25 @@ import cors from "cors";
 config();
 const app = express();
 
+
+const allowedOrigins = [
+    'https://ai-chat-bot-front-jt804p9g7-atul-kumar-pandeys-projects.vercel.app',
+    'https://ai-chat-bot-front-end.vercel.app',
+    'https://ai-chat-bot-front-jt804p9g7-atul-kumar-pandeys-projects.vercel.app'
+];
+
+app.use(cors({
+    origin: function(origin, callback) {
+      // Check if the request origin is in the allowed origins list
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+}));
 // middlewares
-app.use(cors({ origin: "*", credentials: true }));
+// app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
