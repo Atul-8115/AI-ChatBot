836d606b0ccaee504282fefdc374a399378bfe5c
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-05-25 19:30:53 +0530

    refactor: Update CORS origin configuration to allow all origins

--- src/app.ts
+++ src/app.ts
@@ -8,27 +8,29 @@ import cors from "cors";
 config();
 const app = express();
 
-const allowedOrigins = [
-    'https://ai-chat-bot-front-jt804p9g7-atul-kumar-pandeys-projects.vercel.app',
-    'https://ai-chat-bot-front-end.vercel.app',
-    'https://ai-chat-bot-front-jt804p9g7-atul-kumar-pandeys-projects.vercel.app'
-];
+// const allowedOrigins = [
+//     'https://ai-chat-bot-front-jt804p9g7-atul-kumar-pandeys-projects.vercel.app',
+//     'https://ai-chat-bot-front-end.vercel.app',
+//     'https://ai-chat-bot-front-jt804p9g7-atul-kumar-pandeys-projects.vercel.app'
+// ];
 
 app.get("/", (req,res) => {
   return res.send("Hello World.")
 })
 
-app.use(cors({
-    origin: function(origin, callback) {
-      // Check if the request origin is in the allowed origins list
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true // Allow credentials
-}));
+// app.use(cors({
+//     origin: function(origin, callback) {
+//       // Check if the request origin is in the allowed origins list
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     credentials: true // Allow credentials
+// }));
+
+app.use(cors({origin: "*", credentials: true}))
 
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
